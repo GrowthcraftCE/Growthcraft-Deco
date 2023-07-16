d83b961d60adb0c3a8a92b3fc7a3e9f94b0a9cd0
= <->
2023-07-16 12:13:15 -0500

    Implemented Initial Data Generators

--- src/main/java/growthcraft/deco/shared/Reference.java
+++ src/main/java/growthcraft/deco/shared/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 
     public static final String NAME = "Growthcraft Decorations";
     public static final String NAME_SHORT = "deco";
-    public static final String VERSION = "9.0.0";
+    public static final String VERSION = "9.0.1";
 
     private Reference() { /* Prevent default public constructor */ }
 

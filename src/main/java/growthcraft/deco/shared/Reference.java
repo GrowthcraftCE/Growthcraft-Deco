60e4d052d0013ac675f8d65c236f48e9a927c905
Alatyami <alatyami@gmail.com>
2023-08-22 20:38:11 -0600

    Fixed versioning in jar manifest.

--- src/main/java/growthcraft/deco/shared/Reference.java
+++ src/main/java/growthcraft/deco/shared/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 
     public static final String NAME = "Growthcraft Decorations";
     public static final String NAME_SHORT = "deco";
-    public static final String VERSION = "9.0.1";
+    public static final String VERSION = "9.0.2";
 
     private Reference() { /* Prevent default public constructor */ }
 

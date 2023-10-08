4b7fdc3f9e7329e8c7a024d9e170ad9fe7ffbee7
Alatyami <alatyami@gmail.com>
2023-10-08 16:14:38 -0600

    Added MODID check for missing registry entries.

--- src/main/java/growthcraft/deco/init/GrowthcraftDecoMissingMappings.java
+++ src/main/java/growthcraft/deco/init/GrowthcraftDecoMissingMappings.java
@@ -16,32 +16,32 @@ public class GrowthcraftDecoMissingMappings {
 
     public static void missingMappingEventHandler(MissingMappingsEvent event) {
         event.getAllMappings(ForgeRegistries.Keys.ITEMS).forEach( missingItemMapping -> {
-            switch (missingItemMapping.getKey().toString()) {
-                case "growthcraft_deco:stairs_deepslate_brick_glowing" ->
-                        missingItemMapping.remap(ForgeRegistries.ITEMS.getValue(
-                                        new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_BRICK_GLOWING))
-                        );
-                case "growthcraft_deco:deepslate_tile_stairs_glowing", "growthcraft_deco:stairs_deepslate_tile_glowing" ->
-                        missingItemMapping.remap(ForgeRegistries.ITEMS.getValue(
-                                new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_TILE_GLOWING))
-                        );
-                default ->
-                        throw new IllegalStateException("Unexpected value: " + missingItemMapping.getKey().toString());
+            if(missingItemMapping.getKey().getNamespace() == Reference.MODID) {
+                switch (missingItemMapping.getKey().toString()) {
+                    case "growthcraft_deco:stairs_deepslate_brick_glowing" -> missingItemMapping.remap(ForgeRegistries.ITEMS.getValue(
+                            new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_BRICK_GLOWING))
+                    );
+                    case "growthcraft_deco:deepslate_tile_stairs_glowing", "growthcraft_deco:stairs_deepslate_tile_glowing" ->
+                            missingItemMapping.remap(ForgeRegistries.ITEMS.getValue(
+                                    new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_TILE_GLOWING))
+                            );
+                    default -> throw new IllegalStateException("Unexpected value: " + missingItemMapping.getKey().toString());
+                }
             }
         });
 
         event.getAllMappings(ForgeRegistries.Keys.BLOCKS).forEach( missingBlockMapping -> {
-            switch (missingBlockMapping.getKey().toString()) {
-                case "growthcraft_deco:stairs_deepslate_brick_glowing" ->
-                    missingBlockMapping.remap(ForgeRegistries.BLOCKS.getValue(
+            if(missingBlockMapping.getKey().getNamespace() == Reference.MODID) {
+                switch (missingBlockMapping.getKey().toString()) {
+                    case "growthcraft_deco:stairs_deepslate_brick_glowing" -> missingBlockMapping.remap(ForgeRegistries.BLOCKS.getValue(
                             new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_BRICK_GLOWING))
                     );
-                case "growthcraft_deco:deepslate_tile_stairs_glowing", "growthcraft_deco:stairs_deepslate_tile_glowing" ->
-                    missingBlockMapping.remap(ForgeRegistries.BLOCKS.getValue(
-                            new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_TILE_GLOWING))
-                    );
-                default ->
-                        throw new IllegalStateException("Unexpected value: " + missingBlockMapping.getKey().toString());
+                    case "growthcraft_deco:deepslate_tile_stairs_glowing", "growthcraft_deco:stairs_deepslate_tile_glowing" ->
+                            missingBlockMapping.remap(ForgeRegistries.BLOCKS.getValue(
+                                    new ResourceLocation(Reference.MODID, Reference.UnlocalizedName.STAIR_DEEPSLATE_TILE_GLOWING))
+                            );
+                    default -> throw new IllegalStateException("Unexpected value: " + missingBlockMapping.getKey().toString());
+                }
             }
         });
     }
